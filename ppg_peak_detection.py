76174176a7739eead31f1642b3dc1de371ec9d10
Victor O. Costa <victor-o.c@hotmail.com>
2019-09-19 12:44:36 +0000

    Implement confusion matrix, regularization and total cost (untested)

--- ppg_peak_detection.py
+++ ppg_peak_detection.py
@@ -13,7 +13,7 @@ class crossover_detector:
         self.average_fast = 0.0
         self.average_slow = 0.0
         self.crossover_index = 0.0
-        
+    
     def set_parameters(self, alpha_fast, alpha_slow):
         """ Define exponential average and threshold parameters """ 
         self.alpha_fast = alpha_fast
@@ -78,49 +78,118 @@ class crossover_detector:
             
         return heart_rates
         
-    def _group_signal(signal, fs, time):
-        """ Groups a given signal in n items at a time, being n dependent on the sampling frequency (Hz) and wanted time (s) """
-        n = fs * time
-        iterable_groups = zip(*([iter(signal)]*n))
-        return iterable_groups
-        
-    def _group_hr_reference(heart_rates, minute):
-        """ Groups reference hrv in a list according to the wanted minute (eg. minute = 0, returns hrv from 00:00 to 01:00) """
-        sum = 0.0
-        grouped_hr = []
-        for value in heart_rates:
-            sum += value
-            if sum > 60*minute and sum < 60*(minute+1):
-                grouped_hr.append(value)
-                
-        return grouped_hr
+
     
 
-    def signal_regularized_cost(self, detected_peaks, peaks_reference, C, fs):
-    """ Given a set of detected peaks, peaks reference and a constant C, returns the cost based on the confusion matrix regularized by total prediction area and number of predicted peaks.
-        The number of TPs, TNs, FPs and FNs is considered according to the predictors point of view: For each sequence of equally classified samples, the class of this samples block in the confusion matrix is evaluated.
-        Implements a regularization term via a sum of two terms:
-        - (number of positively predicted samples)]/(samples number) to make the total prediction area small
-        - |(actual number of peaks) - (predicted number of peaks)| / max(actual, predicted) to make the predicted number of peaks close to the actual number"""
-    
-        tp = 0              # True positives
-        tn = 0              # True negatives
-        fp = 0              # False positives
-        fn = 0              # False negatives
+    def signal_confusion_matrix(self, detected_peaks, peaks_reference, C):
+        """ Given a set of detected peaks and peaks reference, returns the confusion matrix."""
+        # Confusion matrix:
+        # Between a falling and a rising edge: for 0 reference peaks, TN++;    for N reference peaks, FN += N
+        # Between a rising and a falling edge: for N reference peaks, TP += N; for 0 reference peaks, FP++
+        # In this way, TP + FN = len(peaks_reference)
+        true_positives = 0 
+        true_negatives = 0 
+        false_positives = 0
+        false_negatives = 0
+        
+        # Assumes that the detected_peaks array treats 0 as negative prediction and 1 as positive prediction
+        state = 0                                                       # Initial state is negative prediction (no peak)
+        state_peaks = 0                                                 # Number of peaks for a given state 
+        ref_index = 0                                                   # Index of the reference peaks array
+        for index, prediction in enumerate(detected_peaks):
+            # Updates confusion matrix when reaches an edge 
+            if prediction != state:
+                # Rising edge
+                if state == 0:
+                    if state_peaks == 0:
+                        #print("True negative at index = ", index) 
+                        true_negatives += 1
+                    else:
+                        #print("False negative at index = ", index) 
+                        false_negatives += state_peaks
+                
+                # Falling edge
+                elif state == 1:
+                    if state_peaks == 0:
+                        #print("False positive at index = ", index) 
+                        false_positives += 1
+                    else:
+                        #print("True positive at index = ", index) 
+                        true_positives += state_peaks
+                    
+                state_peaks = 0
+                state = prediction
+                
+            # Checks if a peak ocurred at the current position
+            if index == peaks_reference[ref_index]:
+                state_peaks += 1
+                if ref_index < len(peaks_reference) - 1:
+                    ref_index += 1
+                
+        return true_positives, true_negatives, false_positives, false_negatives
         
         
-        return cost
+    def signal_regularization(self, detected_peaks, peaks_reference):
+        """ Given a set of detected peaks and peaks reference, returns the regularization value, considering total prediction area and number of predicted peaks. """        
+        # Assumes that detected_peaks treats 0 as negative prediction and 1 as positive prediction
+        # Considers the proportion between positively predicted and total prediction area to make the positive prediction area small
+        positive_predictions = sum(detected_peaks)
+        total_predictions = len(detected_peaks)
+        area_term = positive_predictions/total_predictions                                                                          # Maximum area_term value is 1
         
-    def total_regularized_cost(self, ppg_records, C, fs):
-        """ Given a set of PPG signals and the correspondent peak references, calculates a confusion matrix-based metric, regularized by the total area and number of peaks detected.  """
+        # Considers the number of detected peaks and reference peaks to make them close
+        detected_peaks = np.array(detected_peaks)
+        rising_edges_mask = np.flatnonzero((detected_peaks[:-1] == 0) & (detected_peaks[1:] == 1))                                  # Mask to get the rising edges indices
+        predicted_peaks_number = len(rising_edges_mask)
+        reference_peaks_number = len(peaks_reference)
+        number_term = abs(reference_peaks_number - predicted_peaks_number)/max(reference_peaks_number, predicted_peaks_number)      # Maximum number_term value is 1 
+        
+        # Total regularization is the average between area and number terms, having a maximum value of 1
+        total_regularization = (area_term + number_term)/2
+        
+        return total_regularization
+        
+    def total_regularized_cost(self, ppg_records, C):
+        """ Given a set of PPG records cont and the correspondent peak references, calculates a confusion matrix-based metric, regularized by the total area and number of peaks detected.  """
         total_cost = 0.0
         for record in ppg_records:
-            _, _, _, detected_peaks = self.detect_peaks(record.ppg)
-            record_cost = self.signal_regularized_cost(detected_peaks, record.peaks, C, fs)
+            ppg_signal = record.ppg[1]
+            reference_peaks = np.array(record.hrv[0]) - record.ppg[0][0]            # Shifts reference peaks so it is in phase with ppg_signal
+            
+            # Detect peaks using current set of parameters
+            _, _, _, detected_peaks = self.detect_peaks(ppg_signal)
+            
+            # Get record's confusion matrix and regularization term
+            tp, tn, fp, fn = self.signal_confusion_matrix(detected_peaks, reference_peaks)
+            record_regularization= self.signal_regularization(detected_peaks, reference_peaks)
+            
+            # Calculate record's accuracy and cost
+            record_accuracy = (tp + tn)/(tp + tn + fp + fn)
+            record_cost = record_accuracy + C * (record_regularization)
+            total_cost += record_cost
+            
+        total_cost =/ len(ppg_records)
         
         return total_cost
         
     # DEPRECATED RMSE OF FEATURES BY MINUTE
+    # def _group_signal(signal, fs, time):
+        # """ Groups a given signal in n items at a time, being n dependent on the sampling frequency (Hz) and wanted time (s) """
+        # n = fs * time
+        # iterable_groups = zip(*([iter(signal)]*n))
+        # return iterable_groups
+        
+    # def _group_hr_reference(heart_rates, minute):
+        # """ Groups reference hrv in a list according to the wanted minute (eg. minute = 0, returns hrv from 00:00 to 01:00) """
+        # sum = 0.0
+        # grouped_hr = []
+        # for value in heart_rates:
+            # sum += value
+            # if sum > 60*minute and sum < 60*(minute+1):
+                # grouped_hr.append(value)
+                
+        # return grouped_hr
+    
     # def features_rmse(self, ppg_signals, hr_references, fs, features_extractor):
         # """ Returns RMSE of the estimated hrv parameters by minute on each subject using given hrv references """
         # total_error = 0.0
