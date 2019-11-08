Fix variance peak detector optimization

apply_peak_detection.py
@@ -9,9 +9,9 @@ import matplotlib.pyplot as plt
 sample_record = records[10]
 sample_signal = sample_record.ppg[1]
 sample_peaks = np.array(sample_record.beats[0]) - sample_record.ppg[0][0] 
-
-# Apply detector
 detector = crossover_detector()
+
+## Apply crossover detector
 # #detector.set_parameters(alpha_fast = 0.8329186816539544, alpha_slow = 0.9463972836760943)                   # Current best set (17/10/19), with cost of 0.4324561691187358
 # detector.set_parameters(alpha_fast = 0.9684424413846544, alpha_slow = 0.9689074349612342)                    # Current best set of husm (07/11/19), with cost of 0.95
 # fast_averages, slow_averages, crossover_indices, detected_peaks = detector.detect_peaks(sample_signal)
@@ -34,16 +34,34 @@ detector = crossover_detector()
 
 # plt.show()
 
-detector.set_parameters_var(var_alpha = 0.002, avg_alpha = 0.9, var_threshold = 100.0)
-variances, averages, detected_peaks = detector.detect_peaks_var(sample_signal);
+## Apply variance detector
+# detector.set_parameters_var(var_alpha = 0.002, avg_alpha = 0.9, var_threshold = 100.0)
+# variances, averages, detected_peaks = detector.detect_peaks_var(sample_signal);
+# # Plot signal and reference
+# plt.figure()
+# plt.title("PPG average and variance")
+# plt.plot(sample_signal, color='k', label="PPG signal")
+# plt.scatter(sample_peaks, [1]*len(sample_peaks), label="Reference peaks")
+# plt.plot(detected_peaks, color = 'y', label = "Detected peaks")
+# plt.plot(averages, color='magenta', label="average")
+# plt.plot(variances, color='navy', label="variance")
+
+# plt.legend()
+# plt.show()
+
+
+## Apply Mixed detector
+detector.set_parameters_mix(alpha_fast = 0.9656528722478156 , alpha_slow = 0.9668991435014739, 
+                            var_alpha = 0.8322917528906034, avg_alpha = 0.18278088860663844, var_threshold = 21.16502812624924)
+detected_peaks = detector.detect_peaks_mix(sample_signal);
 # Plot signal and reference
 plt.figure()
 plt.title("PPG average and variance")
 plt.plot(sample_signal, color='k', label="PPG signal")
 plt.scatter(sample_peaks, [1]*len(sample_peaks), label="Reference peaks")
 plt.plot(detected_peaks, color = 'y', label = "Detected peaks")
-plt.plot(averages, color='magenta', label="average")
-plt.plot(variances, color='navy', label="variance")
+#plt.plot(averages, color='magenta', label="average")
+#plt.plot(variances, color='navy', label="variance")
 
 plt.legend()
 plt.show()

ppg_peak_detection.py
@@ -2,6 +2,7 @@
 # Author: Victor O. Costa 
 
 import numpy as np
+import sys
 
 class crossover_detector:
     """ Class to process the PPG signal and indicate peaks using a crossover of moving averages """
@@ -301,7 +302,7 @@ class crossover_detector:
         
         return area_term
         
-    def total_regularized_cost(self, ppg_records, C):
+    def total_regularized_cost(self, ppg_records, C, method):
         """ Given a set of PPG records cont and the correspondent peak references, calculates a confusion matrix-based metric, regularized by the total area and number of peaks detected.  """
         total_cost = 0.0
         for index, record in enumerate(ppg_records):
@@ -310,18 +311,23 @@ class crossover_detector:
             reference_peaks = np.array(record.beats[0]) - record.ppg[0][0]            # Shifts reference peaks so it is in phase with ppg_signal
             
             # Detect peaks using current set of parameters
-            _, _, _, detected_peaks = self.detect_peaks(ppg_signal)
-            
+            if method == 'crossover':
+                _, _, _, detected_peaks = self.detect_peaks(ppg_signal)
+            elif method == 'variance':
+                _, _, detected_peaks = self.detect_peaks_var(ppg_signal)
+            elif method == 'mix':
+                detected_peaks = self.detect_peaks_mix(ppg_signal)
+            else:
+                print("No match for method argument in total cost")
+                sys.exit(-1)
+                
             # Get record's confusion matrix and regularization term
             tp, tn, fp, fn, _ = self.signal_confusion_matrix(detected_peaks, reference_peaks)
 
-            #if((tp <= 0) or (tn <= 0) or (fp <= 0) or (fn <= 0)):
-            print('[RECORD ', index,']')
+            #print('[RECORD ', index,']')
             print('TP: ', tp, 'TN: ', tn, 'FP: ', fp, 'FN: ', fn)
-            print('Number of reference peaks: ', len(reference_peaks))
+            #print('Number of reference peaks: ', len(reference_peaks))
             
-            #/if
-
             record_regularization = self.signal_regularization(detected_peaks, reference_peaks)
             
             # Calculate record's accuracy and cost

random_search_parameters.py
@@ -31,7 +31,7 @@ try:
     C = 0                                         # Regularization hyperparameter
     print('\nC = ' + str(C))
 
-    num_iterations = 2000                            # Number of random search iterations
+    num_iterations = 10000                            # Number of random search iterations
     print('\nnum_iterations = ' + str(num_iterations))
 
     
@@ -46,7 +46,7 @@ try:
         # alpha_fast = np.random.uniform(0, 1)
         # alpha_slow = np.random.uniform(alpha_fast, 1)
         # peak_detector.set_parameters(alpha_fast, alpha_slow)
-        # cost = peak_detector.total_regularized_cost(train_records, C)
+        # cost = peak_detector.total_regularized_cost(train_records, C, "crossover")
         # print('[randomized] alpha_fast: ', alpha_fast, ', alpha_slow: ', alpha_slow,', cost: ', cost)
         # # Keep solutions in a matrix
         # if iteration == 0:
@@ -60,9 +60,9 @@ try:
         # Randomize parameters
         var_alpha = np.random.uniform(0,1)
         avg_alpha = np.random.uniform(0,1)
-        var_threshold = np.random.uniform(0,200)
+        var_threshold = np.random.uniform(0,300)
         peak_detector.set_parameters_var(var_alpha, avg_alpha, var_threshold)
-        cost = peak_detector.total_regularized_cost(train_records, C)
+        cost = peak_detector.total_regularized_cost(train_records, C, 'variance')
         print('[randomized] var_alpha: ', var_alpha, ', avg_alpha: ', avg_alpha, 'threshold: ', var_threshold, ', cost: ', cost)
         if iteration == 0:
             best_solution = [var_alpha, avg_alpha, var_threshold, cost]
@@ -71,19 +71,19 @@ try:
         print('[current best] var_alpha: ', best_solution[0], ', avg_alpha: ', best_solution[1], 'threshold: ', best_solution[2], ', cost: ', best_solution[-1])
         
         ## Optimize mixed
-        alpha_fast = np.random.uniform(0, 1)
-        alpha_slow = np.random.uniform(alpha_fast, 1)
-        var_alpha = np.random.uniform(0,1)
-        avg_alpha = np.random.uniform(0,1)
-        var_threshold = np.random.uniform(0,100)
-        peak_detector.set_parameters_mix(alpha_fast, alpha_slow, var_alpha, avg_alpha, var_threshold)
-        cost = peak_detector.total_regularized_cost(train_records, C)
-        print('[randomized] alpha_fast: ', alpha_fast, ', alpha_slow: ', alpha_slow, 'var_alpha: ', var_alpha, ', avg_alpha: ', avg_alpha, 'threshold: ', var_threshold, ', cost: ', cost)
-        if iteration == 0:
-            best_solution = [alpha_fast, alpha_slow, var_alpha, avg_alpha, var_threshold, cost]
-        elif cost < best_solution[-1]:
-            best_solution = [alpha_fast, alpha_slow, var_alpha, avg_alpha, var_threshold, cost]
-        print('[current best] alpha_fast: ', best_solution[0], ', alpha_slow: ', best_solution[1], 'var_alpha: ', best_solution[2], ', avg_alpha: ', best_solution[3], 'threshold: ', best_solution[4], ', cost: ', best_solution[-1])
+        # alpha_fast = np.random.uniform(0.9, 1)
+        # alpha_slow = np.random.uniform(alpha_fast, 1)
+        # var_alpha = np.random.uniform(0,1)
+        # avg_alpha = np.random.uniform(0,1)
+        # var_threshold = np.random.uniform(0,100)
+        # peak_detector.set_parameters_mix(alpha_fast, alpha_slow, var_alpha, avg_alpha, var_threshold)
+        # cost = peak_detector.total_regularized_cost(train_records, C, 'mix')
+        # print('[randomized] alpha_fast: ', alpha_fast, ', alpha_slow: ', alpha_slow, 'var_alpha: ', var_alpha, ', avg_alpha: ', avg_alpha, 'threshold: ', var_threshold, ', cost: ', cost)
+        # if iteration == 0:
+            # best_solution = [alpha_fast, alpha_slow, var_alpha, avg_alpha, var_threshold, cost]
+        # elif cost < best_solution[-1]:
+            # best_solution = [alpha_fast, alpha_slow, var_alpha, avg_alpha, var_threshold, cost]
+        # print('[current best] alpha_fast: ', best_solution[0], ', alpha_slow: ', best_solution[1], 'var_alpha: ', best_solution[2], ', avg_alpha: ', best_solution[3], 'threshold: ', best_solution[4], ', cost: ', best_solution[-1])
         
     # Sort solutions according to the costs
     #solution_archive = solution_archive[solution_archive[:,-1].argsort()]