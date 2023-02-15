f11ab26494cf7d924c55f5b18f4eb2e89a366be6
Victor O. Costa <victor-o.c@hotmail.com>
2023-02-15 17:18:00 -0300

    Modify visual aspects of per-subject metric comparison between our crossover and TERMA

--- paper_plots/plot_results.py
+++ paper_plots/plot_results.py
@@ -29,34 +29,34 @@ import matplotlib.pyplot as plt
 
 # Loads arrays with precision and recall values from cross-validation
 ## Validation results for the proposed model
-val_precisions_this = np.load('../search_results/LOSOCV_RS_22folds_30runs_precisions.npy')
-val_recalls_this = np.load('../search_results/LOSOCV_RS_22folds_30runs_recalls.npy')
+precisions_this = np.load('../search_results/LOSOCV_RS_crossover_22folds_30runs_precisions.npy')
+recalls_this = np.load('../search_results/LOSOCV_RS_crossover_22folds_30runs_recalls.npy')
 
 ## Validation results for TERMA
-val_precisions_terma = np.load('../search_results/TERMA_GS_LOSOCV_22folds_precisions.npy')
-val_recalls_terma = np.load('../search_results/TERMA_GS_LOSOCV_22folds_recalls.npy')
+precisions_terma = np.load('../search_results/LOSOCV_GS_TERMA_22folds_precisions.npy')
+recalls_terma = np.load('../search_results/LOSOCV_GS_TERMA_22folds_recalls.npy')
 
 # Dimensionality checking
 print('Expected dimensions for our results: (22, 30, # of IOI)')
-print(f'Actual dimensions (P): {np.shape(val_precisions_this)}')
-print(f'Actual dimensions (R): {np.shape(val_recalls_this)}')
+print(f'Actual dimensions (P): {np.shape(precisions_this)}')
+print(f'Actual dimensions (R): {np.shape(recalls_this)}')
 
 print('\nExpected dimensions for TERMA\'s results: (22)')
-print(f'Actual dimensions (P): {np.shape(val_precisions_terma)}')
-print(f'Actual dimensions (R): {np.shape(val_recalls_terma)}')
+print(f'Actual dimensions (P): {np.shape(precisions_terma)}')
+print(f'Actual dimensions (R): {np.shape(recalls_terma)}')
 
 # Split data, considering origin method, health condition and metric
 ## TERMA 
-pre_copd_terma = val_precisions_terma[0:11]
-pre_healthy_terma = val_precisions_terma[11:22]
-rec_copd_terma = val_recalls_terma[0:11]
-rec_healthy_terma = val_recalls_terma[11:22]
+pre_copd_terma = precisions_terma[0:11]
+pre_healthy_terma = precisions_terma[11:22]
+rec_copd_terma = recalls_terma[0:11]
+rec_healthy_terma = recalls_terma[11:22]
 
 ## THIS
-pre_copd_this = np.sum(val_precisions_this[0:11,:,-1], axis=1) / 30
-pre_healthy_this = np.sum(val_precisions_this[11:22,:,-1], axis=1) / 30
-rec_copd_this = np.sum(val_recalls_this[0:11,:,-1], axis=1) / 30
-rec_healthy_this = np.sum(val_recalls_this[11:22,:,-1], axis=1) / 30
+pre_copd_this = np.sum(precisions_this[0:11,:,-1], axis=1) / 30
+pre_healthy_this = np.sum(precisions_this[11:22,:,-1], axis=1) / 30
+rec_copd_this = np.sum(recalls_this[0:11,:,-1], axis=1) / 30
+rec_healthy_this = np.sum(recalls_this[11:22,:,-1], axis=1) / 30
 
 # Summary statistics to check for correctness
 print('\nSTATS FOR TERMA')
@@ -72,67 +72,71 @@ print(f'Healthy precision: {np.mean(pre_healthy_this)} {np.std(pre_healthy_this,
 print(f'Healthy recall: {np.mean(rec_healthy_this)} {np.std(rec_healthy_this, ddof=1)}')
 
 # There are 4 plots, from the cartesian product (precision, recall)x(healthy, COPD)
-bar_width = 0.35
+bar_width = 0.4
+bar_offset = bar_width/2 + 0.02
+y_max = 1.01
+y_min = 0.9
+color_this = 'mediumseagreen'
+color_TERMA = 'slateblue'
+figure_proportion = (15, 5)
+
 subj_ticks = np.arange(1,12)
 healthy_labels = ['H'+r'$_1$', 'H'+r'$_2$', 'H'+r'$_3$', 'H'+r'$_4$', 'H'+r'$_5$', 'H'+r'$_6$',
                   'H'+r'$_7$', 'H'+r'$_8$', 'H'+r'$_9$', 'H'+r'$_{10}$', 'H'+r'$_{11}$']
 COPD_labels = ['C'+r'$_1$', 'C'+r'$_2$', 'C'+r'$_3$', 'C'+r'$_4$', 'C'+r'$_5$', 'C'+r'$_6$',
                'C'+r'$_7$', 'C'+r'$_8$', 'C'+r'$_9$', 'C'+r'$_{10}$', 'C'+r'$_{11}$']
 
-## (Precision, healthy)
-fig, axs = plt.subplots(1, figsize=(15, 8))
-barlist_this = axs.bar(subj_ticks, pre_healthy_this, width = bar_width, label='This')
-barlist_terma = axs.bar(subj_ticks + bar_width, pre_healthy_terma, width = bar_width, label='TERMA')
-[ bar.set_color('#DE8484') for bar in barlist_this ]
-[ bar.set_color('#92BD77') for bar in barlist_terma ]
 
-axs.set_title('Precisions for healthy subjects')
-axs.set_ylim([0.94, 1.001])
+## (Precision, healthy)
+fig, axs = plt.subplots(1, figsize = figure_proportion)
+barlist_this = axs.bar(subj_ticks - bar_offset, pre_healthy_this, width = bar_width, label='This')
+barlist_terma = axs.bar(subj_ticks + bar_offset, pre_healthy_terma, width = bar_width, label='TERMA')
+[ bar.set_color(color_this) for bar in barlist_this ]
+[ bar.set_color(color_TERMA) for bar in barlist_terma ]
+axs.set_title('Precisions for healthy subjects', fontsize=18)
+axs.set_ylim([y_min, y_max])
 axs.set_xticks(subj_ticks)
 axs.set_xticklabels(healthy_labels)
 axs.tick_params(axis='both', which='major', labelsize=16)
-# plt.savefig(f'')	
+plt.savefig(f'precisions_healthy_subs.png', bbox_inches='tight')	
 
 ## (Precision, COPD)
-fig, axs = plt.subplots(1, figsize=(15, 8))
-barlist_this = axs.bar(subj_ticks, pre_copd_this, width = bar_width, label='This')
-barlist_terma = axs.bar(subj_ticks + bar_width, pre_copd_terma, width = bar_width, label='TERMA')
-[ bar.set_color('#DE8484') for bar in barlist_this ]
-[ bar.set_color('#92BD77') for bar in barlist_terma ]
-
-axs.set_title('Precisions for COPD patients')
-axs.set_ylim([0.94, 1.001])
+fig, axs = plt.subplots(1, figsize = figure_proportion)
+barlist_this = axs.bar(subj_ticks - bar_offset, pre_copd_this, width = bar_width, label='This')
+barlist_terma = axs.bar(subj_ticks + bar_offset, pre_copd_terma, width = bar_width, label='TERMA')
+[ bar.set_color(color_this) for bar in barlist_this ]
+[ bar.set_color(color_TERMA) for bar in barlist_terma ]
+axs.set_title('Precisions for COPD patients', fontsize=18)
+axs.set_ylim([y_min, y_max])
 axs.set_xticks(subj_ticks)
 axs.set_xticklabels(COPD_labels)
 axs.tick_params(axis='both', which='major', labelsize=16)
-# plt.savefig(f'')	
+plt.savefig(f'precisions_copd_subs.png', bbox_inches='tight')	
 
 ## (Recall, healthy)
-fig, axs = plt.subplots(1, figsize=(15, 8))
-barlist_this = axs.bar(subj_ticks, rec_healthy_this, width = bar_width, label='This')
-barlist_terma = axs.bar(subj_ticks + bar_width, rec_healthy_terma, width = bar_width, label='TERMA')
-[ bar.set_color('#DE8484') for bar in barlist_this ]
-[ bar.set_color('#92BD77') for bar in barlist_terma ]
-
-axs.set_title('Recalls for healthy subjects')
-axs.set_ylim([0.94, 1.001])
+fig, axs = plt.subplots(1, figsize = figure_proportion)
+barlist_this = axs.bar(subj_ticks - bar_offset, rec_healthy_this, width = bar_width, label='This')
+barlist_terma = axs.bar(subj_ticks + bar_offset, rec_healthy_terma, width = bar_width, label='TERMA')
+[ bar.set_color(color_this) for bar in barlist_this ]
+[ bar.set_color(color_TERMA) for bar in barlist_terma ]
+axs.set_title('Recalls for healthy subjects', fontsize=18)
+axs.set_ylim([y_min, y_max])
 axs.set_xticks(subj_ticks)
 axs.set_xticklabels(healthy_labels)
 axs.tick_params(axis='both', which='major', labelsize=16)
-# plt.savefig(f'')	
+plt.savefig(f'recalls_healthy_subs.png', bbox_inches='tight')
 
 ## (Recall, COPD)
-fig, axs = plt.subplots(1, figsize=(15, 8))
-barlist_this = axs.bar(subj_ticks, rec_copd_this, width = bar_width, label='This')
-barlist_terma = axs.bar(subj_ticks + bar_width, rec_copd_terma, width = bar_width, label='TERMA')
-[ bar.set_color('#DE8484') for bar in barlist_this ]
-[ bar.set_color('#92BD77') for bar in barlist_terma ]
-
-axs.set_title('Recalls for COPD patients')
-axs.set_ylim([0.94, 1.001])
+fig, axs = plt.subplots(1, figsize = figure_proportion)
+barlist_this = axs.bar(subj_ticks - bar_offset, rec_copd_this, width = bar_width, label='This')
+barlist_terma = axs.bar(subj_ticks + bar_offset, rec_copd_terma, width = bar_width, label='TERMA')
+[ bar.set_color(color_this) for bar in barlist_this ]
+[ bar.set_color(color_TERMA) for bar in barlist_terma ]
+axs.set_title('Recalls for COPD patients', fontsize=18)
+axs.set_ylim([y_min, y_max])
 axs.set_xticks(subj_ticks)
 axs.set_xticklabels(COPD_labels)
 axs.tick_params(axis='both', which='major', labelsize=16)
-# plt.savefig(f'')	
+plt.savefig(f'recalls_copd_subs.png', bbox_inches='tight')	
 
-plt.show()
+# plt.show()
