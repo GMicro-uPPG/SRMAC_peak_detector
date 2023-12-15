8ab569f3d4a64e28fabbd1169161b054a034c248
Victor O. Costa <victor-o.c@hotmail.com>
2023-12-15 16:15:43 -0300

    Update copyright year

--- demonstrate_RS_SRMAC.py
+++ demonstrate_RS_SRMAC.py
@@ -2,7 +2,7 @@
 
 # MIT License
 
-# Copyright (c) 2021 Grupo de Microeletrônica (Universidade Federal de Santa Maria)
+# Copyright (c) 2023 Grupo de Microeletrônica (Universidade Federal de Santa Maria)
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
